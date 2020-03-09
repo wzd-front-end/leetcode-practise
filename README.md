1b6255925135da7e462cec1cc6e40faaad71ba67
wuzhida <429023964@qq.com>
2020-03-09 17:10:12 +0800

    add: 新增两数相加

--- README.md
+++ README.md
@@ -3,22 +3,23 @@
 题库：[https://leetcode-cn.com/problemset/all/](https://leetcode-cn.com/problemset/all/)
 
 ## 已刷题目
-已刷题目总数：1
+已刷题目总数：2
 
 **简单(easy)**[1]
-- 1.两数之和
- 【数组|哈希表
-】——[查看代码](https://github.com/wzd-front-end/leetcode-practise/blob/master/easy/1-two-sum.js) [查看原题](https://leetcode-cn.com/problems/two-sum/
-)
+- 1.两数之和 【数组|哈希表】——[查看代码](https://github.com/wzd-front-end/leetcode-practise/blob/master/easy/1-two-sum.js) [查看原题](https://leetcode-cn.com/problems/two-sum/)
 
-**中等(medium)**[0]
+**中等(medium)**[1]
+- 2.两数相加 【链表|数学】——[查看代码](https://github.com/wzd-front-end/leetcode-practise/blob/master/medium/2-add-two-numbers.js) [查看原题](https://leetcode-cn.com/problems/add-two-numbers/)
 
 **困难(hard)**[0]
 
 
 ## 日期进度
 
+### 2020-03-09【题目数：1】
+
+1.15:17——[2.两数相加](https://github.com/wzd-front-end/leetcode-practise/blob/master/medium/2-add-two-numbers.js)(medium)
+
 ### 2020-03-07【题目数：1】
 
-1.22:04——[1.两数之和
-](https://github.com/wzd-front-end/leetcode-practise/blob/master/easy/1-two-sum.js)(easy)
+1.22:04——[1.两数之和](https://github.com/wzd-front-end/leetcode-practise/blob/master/easy/1-two-sum.js)(easy)
---
+++ medium/2-add-two-numbers.js
@@ -0,0 +1,64 @@
+/**
+ * @name 两数相加
+ * @url https://leetcode-cn.com/problems/add-two-numbers/
+ * @date 2020-03-09 15:17
+ * @tags 链表、数学
+ * description
+ * ```
+  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 
+ * 示例：
+ * 
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * 
+ * 
+ * ```
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val){
+  this.val = val
+  this.next = null
+}
+
+var addTwoNumbers = function(l1, l2) {
+  let dummyHead = new ListNode(0)
+  let p = l1, q = l2, curr = dummyHead
+  let carry = 0
+  while(p !== null || q !== null) {
+    let x = (p !== null) ? p.val : 0
+    let y = (q !== null) ? q.val : 0
+
+    let sum = x + y + carry
+    if(sum >= 10){
+      carry = 1
+    } else{
+      carry = 0
+    }
+    curr.next = new ListNode(sum % 10)
+    curr = curr.next
+    if(p !== null) p = p.next
+    if(q !== null) q = q.next
+  }
+  if(carry > 0){
+    curr.next = new ListNode(carry)
+  }
+  return dummyHead.next
+};
